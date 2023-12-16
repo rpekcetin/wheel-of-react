d62eb68a3b50f69a8a3797874b985f493ad64b20
Recep Muhammet Pekçetin <recepmuhammetpekcetin@Recep-MacBook-Air.local>
2023-12-16 22:36:42 +0300

    add css configuration

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -2801,6 +2801,36 @@ if (process.env.NODE_ENV === 'production') {
 var reactExports = react.exports;
 var React = /*@__PURE__*/getDefaultExportFromCjs(reactExports);
 
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css_248z = "button {\n    background-color: red;\n}";
+styleInject(css_248z);
+
 const Button = (props) => {
     return React.createElement("button", null, props.label);
 };
